26d0a9baf436daeb655637ddd8c6634c2a103ef4
Key <keshawnchristopher@gmail.com>
2022-04-09 20:12:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Keshawn React Portfolio Project
+
+*Fork from [es6-webpack2-starter](https://github.com/micooz/es6-webpack2-starter)*
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,12 @@
+import react, { component } from "react";
+
+export default class PortfolioContainer extends Component 
+{
+    render() {
+        return (
+            <div>
+                <h2>Portfolio items go here...</h2>
+            </div>
+        )
+    }
+}
