ebb88919c333ae4a7476f3eadc0b21541fa09780
Key <keshawnchristopher@gmail.com>
2022-06-16 22:17:37 -0400

    Added Modal feature to Blog

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
+import BlogModal from "../modals/blog-modal";
 
 class Blog extends Component {
   constructor() {
@@ -12,15 +13,30 @@ class Blog extends Component {
       blogItems: [],
       totalCount: 0,
       currentPage: 0,
-      isLoading: true
+      isLoading: true,
+      blogModalIsOpen: false
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
-    this.activateInfiniteScroll();
+    this.onScroll = this.onScroll.bind(this);
+    window.addEventListener("scroll", this.onScroll, false);
+    this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
   }
 
-  activateInfiniteScroll() {
-    window.onscroll = () => {
+  handleModalClose() {
+    this.setState({
+      blogModalIsOpen: false
+    });
+  }
+
+  handleNewBlogClick() {
+    this.setState({
+      blogModalIsOpen: true
+    });
+  }
+
+  onScroll() {
       if (this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
         return;
       }
@@ -32,7 +48,6 @@ class Blog extends Component {
       ) {
         this.getBlogItems();
       }
-    };
   }
 
   getBlogItems() {
@@ -61,6 +76,10 @@ class Blog extends Component {
     this.getBlogItems();
   }
 
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
+  }
+
   render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />
@@ -69,6 +88,17 @@ class Blog extends Component {
 
     return (
         <div className="blog-container">
+        <BlogModal
+        handleModalClose={this.handleModalClose}
+        modalIsOpen={this.state.blogModalIsOpen}
+        />
+
+        <div className="new-blog-link">
+          <a onClick={this.handleNewBlogClick}>
+            Open Modal!
+          </a>
+        </div>
+
           <div className="content-container">{blogRecords}</div>
 
           {this.state.isLoading ? (
